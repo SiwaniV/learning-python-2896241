f3707880b39e06a64094b639493af8997dffd028
SiwaniV <6003716@mborijnland.nl>
2022-10-16 15:57:06 +0200

    edits

--- Ch2 - Basics/loops_start.py
+++ Ch2 - Basics/loops_start.py
@@ -9,16 +9,27 @@ def main():
 
     # TODO: define a while loop
 
+    # while(x < 5):
+    #     print(x)
+    #     x = x + 1
+
 
     # TODO: define a for loop
-
+    
+    # for x in range(5, 10):
+    #     print(x)
+        
 
     # TODO: use a for loop over a collection
-    days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
-
+    # days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+    # for d in days:
+    #     print(d)
 
     # TODO: use the break and continue statements
-
+    for x in range(5, 10):
+        if x == 7:
+            break
+        print(x)
 
     # TODO: using the enumerate() function to get index 
     
